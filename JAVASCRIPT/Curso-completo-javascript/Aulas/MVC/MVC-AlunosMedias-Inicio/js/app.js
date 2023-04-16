9775b36526e4e9c4485ff151133c3b9b7a756bca
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-04-16 12:49:08 -0300

    Estudos

--- JAVASCRIPT/Curso-completo-javascript/Aulas/MVC/MVC-AlunosMedias-Inicio/js/app.js
+++ JAVASCRIPT/Curso-completo-javascript/Aulas/MVC/MVC-AlunosMedias-Inicio/js/app.js
@@ -1,3 +1,4 @@
+// Calcula a média por materia de cada aluno e cria uma propriedade chamada media ↓
 alunos.forEach(aluno => {
     aluno.media = {} // Adiciona a propriedade "media" dentro de aluno em alunos
 
@@ -7,3 +8,31 @@ alunos.forEach(aluno => {
 })
 
 console.log(alunos)
+
+// Inserir no thead "nome" e as materias ↓
+const htmlHeader = document.createElement('tr')
+
+const htmlHeaderMaterias = Object.keys(alunos[0].notas).map((m) => {
+    return `<td>${m}</td>`
+}).join("")
+// Object.keys() recebe um objeto, e retorna um array contendo todas as propriedades do objeto
+/// join() serve para juntar todos os elementos do array em uma única string
+
+htmlHeader.innerHTML = '<td>Nome</td>'
+htmlHeader.innerHTML += htmlHeaderMaterias
+
+document.querySelector('[data-table] thead').appendChild(htmlHeader)
+
+// Percorrer cada aluno do obejto alunos e gerar uma tr com as notas
+
+alunos.forEach((aluno)  => {
+    const htmlBody = document.createElement('tr')
+    let htmlMedias = `<td>${aluno.nome}</td>`
+
+    Object.keys(aluno.notas).forEach((m) => {
+        htmlMedias += `<td>${aluno.media[m]}</td>`
+    })
+
+    htmlBody.innerHTML = htmlMedias
+    document.querySelector('[data-table] tbody').appendChild(htmlBody)
+})

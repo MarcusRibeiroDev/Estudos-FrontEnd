fc42c510125a2fd386d76f399116dea96fdb117c
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-09-11 21:18:04 -0300

    Estudos

--- REACT.JS/Secret_Word/vite-project/src/App.jsx
+++ REACT.JS/Secret_Word/vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import wordsList from './data/words.js'
+import StartScreen from './components/StartScreen'
+//import wordsList from './data/words.js'
 
 function App() {
 
   return (
     <div className='app-container'>
-        <h2>Secret Word</h2>
+        <StartScreen />
     </div>
   )
 }
---
+++ REACT.JS/Secret_Word/vite-project/src/components/StartScreen.jsx
@@ -0,0 +1,15 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+
+        <p>Clique no botão abaixo</p>
+
+        <button>Começar o jogo!</button>
+    </div>
+  )
+}
+
+export default StartScreen

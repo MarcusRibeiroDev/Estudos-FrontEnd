9eb7b868f8a6b480fe2f5831af25795bfac99003
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-09-11 21:00:46 -0300

    Estudos

---
+++ REACT.JS/Secret_Word/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import wordsList from './data/words.js'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+        <h2>Secret Word</h2>
+    </div>
+  )
+}
+
+export default App

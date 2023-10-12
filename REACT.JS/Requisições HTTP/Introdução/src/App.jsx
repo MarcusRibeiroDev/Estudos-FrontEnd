b033e6fd283d27e121699433614699a2b6f31aaf
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-10-12 13:22:42 -0300

    Estudos

--- REACT.JS/Requisições HTTP/Introdução/src/App.jsx
+++ REACT.JS/Requisições HTTP/Introdução/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
 
   const [list, setList] = useState([]) // Lista de produtos
 
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
 
   // 1 - Resgatando Dados
   useEffect(()=>{
@@ -18,17 +20,61 @@ function App() {
     }
 
     fetchList()
-  })
+  }, [])
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+
+    const newProduct = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+    const updatedList = await res.json()
+
+    console.log(updatedList)
+
+    setList((listPreview) => [...listPreview, updatedList])
+
+    setName('')
+    setPrice('')
+  }
 
   return (
     <>
-      <h1>Lista de produtos</h1>
+      <div className='app'>
+        <h1>Lista de produtos</h1>
 
-      <ul>
-        {list.map((p)=>(
-          <li key={p.id}>{p.nome} - R$ {p.preco}</li>
-        ))}
-      </ul>
+        <ul>
+          {list.map((p)=>(
+            <li key={p.id}>{p.name} - R$ {p.price}</li>
+          ))}
+        </ul>
+
+        <hr />
+
+        <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" name="name" value={name} onChange={(e)=> setName(e.target.value)}/>
+            </label>
+
+            <label>
+              Preço:
+              <input type="text" name="price" value={price} onChange={(e)=> setPrice(e.target.value)}/>
+            </label>
+
+            <button type='submit'>Enviar</button>
+        </form>
+      </div>
     </>
   )
 }

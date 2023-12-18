423151c200a8a2e9ac6615b58e9a248d486a28c8
Marcus Ribeiro <marcus10dev@gmail.com>
2023-12-18 20:49:16 -0300

    Delete project

--- REACT.JS/Miniblog/miniblog/src/hooks/useInsertDocument.js
+++
@@ -1,9 +0,0 @@
-import { db } from '../firebase/config' // Import necessário para o firebase
-import { useState, useEffect,useReducer } from 'react'
-import { collection, addDoc, Timestamp } from 'firebase/firestore'
-
-const initialState = {
-
-}
-
-// PAREI NA ULTIMA AULA PARA ENTENDE O HOOK USEREDUCER
